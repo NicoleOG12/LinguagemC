546184c82a3977e749852afd55c7e63c853d9102
nicole <nicole.goncalves6@etec.sp.gov.br>
2023-09-22 16:24:09 -0300

    LinguagemC

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf ("Boa Tarde\n");
+	printf ("Prova na proxima semana\n");
+	printf ("Prova Manuscrita\n");
+	printf ("Sem consulta\n");
+	
+	return 0;
+}
